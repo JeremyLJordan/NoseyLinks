e987c420b9baadccd6b7dbbfd8bd22d2e1f53a39
Jeremy Jordan <jeremyjordanlsus@gmail.com>
2020-08-17 20:52:20 -0500

    Setup File Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Nosey Links',
+    version='0.1',
+    author='N3ON',
+    Twitter='@N3ON_ONE',
+    description='Enumerates common url shorteners by generating random urls and getting the websites title information',
+    install_requires=['argoarse', 'bs4', 'urllib3', 're', 'string', 'random'],
+    py_modules=['NoseyLinks'],
+)
